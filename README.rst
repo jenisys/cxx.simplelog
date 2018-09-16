325918bf404a39016333c55323c6a73b27e06a30
jenisys <jenisys@users.noreply.github.com>
2018-09-16 02:13:07 +0200

    UPDATED: Core ideas and concepts for CXXLOG..., inspect some other logging libs.

--- README.rst
+++ README.rst
@@ -5,12 +5,28 @@ CORE IDEAS:
 
 * Provide common set of preprocessor macros for logging (and tracing)
 * Log engine/subsystem is hidden and can be replaced w/ another implementation
-* Log setup can be specific
+* Log setup can be specific w/ concrete logging subsystem
+
+ASPECTS:
+* log-user: log statements are used in many places
+* log-user: Does not care about setup of logging subsystem and threshold levels
+* log-admin: Setup of logging subsystem/backend
+* log-admin: Setup of log levels, log-sinks, formatters, ...
+* log-admin: Done only in main/main.cpp context (decision: Per program)
+
+INTENTIONS: 
+* Keep log-user code clean (meaning: independent of the concrete logging subsystem)
+* log-user: Recompile of library w/ different logging subsystem is acceptable
+* Support multiple logging subsystems/backends (if possible)
+* Avoid vendor-lockin (of concrete logging subsystem)
+
+NEEDS:
+* Support for multiple categories/loggers per program 
+  (reason: configuration-point for threshold, context for functionality)
 
 PROBLEMS:
-
 * Log with parameters / placeholders: 
-  vargs, iostream-like, format-like, ...
+  printf-like/vargs, iostream-like, format-like, ...
 
 
 DESIGN DECISION:
@@ -26,13 +42,22 @@ SEE ALSO:
 * https://github.com/fmtlib/fmt
 * https://github.com/gabime/spdlog
 
+RELATED: Overview C++ logging libraries
+* https://github.com/rigtorp/awesome-modern-cpp
+* https://cpp.libhunt.com Overview C++ logging libraries
+
+OPEN ISSUE: spdlog
+* Define/Redefine default sink
+* Read logging configuration from config-file
+
+
 
 EXAMPLE
 ------------------------------------------------------------------------------
 
 .. code-block: c++
 
-    auto log = CXXLOG_DEFINE_CATEGORY("hello");
+    CXXLOG_DEFINE_CATEGORY(log, "foo.bar");
     
     // -- MACROS:
     // SAME AS: CXXLOG_ERROR()
@@ -51,4 +76,38 @@ EXAMPLE
 
     // -- CASE: Log message w/ parameters
     std::string name("Alice");
-    LOG_ERROR_F(log4me, "Hello {0} (number={1:d}", name, 42);
+    LOG_ERROR_F(log, "Hello {0} (number={1:d}", name, 42);
+
+
+SCRATCHPAD: C++ Logging Libraries
+-----------------------------------------------------------------------------
+
+SEE ALSO:
+* https://cpp.libhunt.com/categories/779-logging
+
+LIBS:
+* spdlog (license: BSD)
+* boost.log
+* log4cpp
+* ...
+
+* blackhole (license: MIT): https://github.com/3Hren/blackhole
+    - Blackhole is an attribute-based logger with strong focus on gaining maximum performance as possible for such kind of loggers.
+    - Was a header-only lib, is now a shared lib.
+    - Support fmt library !?!
+    - Formatter: JSON, ...
+    - Sinks: Null, Console, File, Socket, ...
+
+* loguru: https://github.com/emilk/loguru
+    - header-only library
+    - Only one log category
+    - Supports as option also fmtlib::fmt
+
+* reckless (license: MIT): https://github.com/mattiasflodin/reckless
+
+BROWSING:
+* templog (license: Boost): http://www.templog.org
+    - Stream-based
+
+* logog (license: BSD-like): https://github.com/johnwbyrd/logog
+    - 
--- example_main.cpp
+++ example_main.cpp
@@ -13,6 +13,7 @@
 #include <spdlog/logger.h>
 #include <spdlog/sinks/stdout_sinks.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <iostream>
 
 
 // ==========================================================================
@@ -25,29 +26,66 @@ namespace {
 }; // namespace
 
 // #define _CXXLOG_IMPL_DEFINE_CATEGORY(name) ::spdlog::logger(name, CXXLOG_DEFAULT_SINK_PTR))
+auto spdlog_getDefaultSink(void)
+{
+    static ::spdlog::sink_ptr theSink(new ::spdlog::sinks::stdout_sink_mt());
+    return theSink;
+}
 auto spdlog_useOrCreateLogger(std::string name)
 {
     auto logPtr = spdlog::get(name);
     if (!logPtr) {
-        logPtr = ::spdlog::stdout_logger_mt(name);
+        // logPtr = ::spdlog::stdout_logger_mt(name);
+        logPtr.reset(new ::spdlog::logger(name, spdlog_getDefaultSink()));
+        spdlog::register_logger(logPtr);
     }
     return std::move(logPtr);
 }
 
+#define _CXXLOG_NUMARGS(...) (sizeof((int[]){__VA_ARGS__}) / sizeof(int))
 #define _CXXLOG_IMPL_DEFINE_CATEGORY(vname, name) auto vname = spdlog_useOrCreateLogger(name)
+#define _CXXLOG_IMPL_DEFINE_STATIC_CATEGORY(vname, name) static _CXXLOG_IMPL_DEFINE_CATEGORY(vname, name)
 #define _CXXLOG_IMPL_LOG(logger, level, format, ...) logger->log(level, format, __VA_ARGS__)
-#define _CXXLOG_IMPL_LOG0(logger, level, message) logger->log(level, message)
+#define _CXXLOG_IMPL_LOG0(logger, level, message)    logger->log(level, message)
 
 #define _CXXLOG_IMPL_LEVEL_ERROR ::spdlog::level::err
 #define _CXXLOG_IMPL_LEVEL_WARN ::spdlog::level::warn
 #define _CXXLOG_IMPL_LEVEL_INFO ::spdlog::level::info
+
+// -- LOGGING SUBSYSTEM IMPL UTILTIES:
+void spdlog_useSinkAsDefaultSink(std::shared_ptr<spdlog::logger> theLog)
+{
+    auto defaultSink = theLog->sinks().front();
+    spdlog::apply_all([&](std::shared_ptr<spdlog::logger> logPtr) {
+        std::cout << "UseDefaultSink with logger="<< logPtr->name() << std::endl;
+        if (logPtr != theLog)
+        {
+            // -- REASSIGN: Log-sink.
+            std::cout << "UseDefaultSink with reassign for: " << logPtr->name() << std::endl;
+            logPtr->sinks().clear();
+            logPtr->sinks().push_back(defaultSink);
+        }
+    });
+}
+
+void spdlog_setLevelToAll(spdlog::level::level_enum threshold)
+{
+    spdlog::apply_all([&](std::shared_ptr<spdlog::logger> log) {
+    	auto levelBefore = log->level();
+		log->set_level(threshold);
+        std::cout << "SetLevel: "<< log->name()
+        		  <<".level="<< log->level()
+				  <<" (was: "<< levelBefore <<")" << std::endl;
+    });
+}
 
 
 // ==========================================================================
 // GENERIC LOGGING MACROS
 // ==========================================================================
 #define CXXLOG_DEFINE_CATEGORY(vname, name)        _CXXLOG_IMPL_DEFINE_CATEGORY(vname, name)
-#define CXXLOG_ERROR(logger, format, ...)   _CXXLOG_IMPL_LOG(logger, _CXXLOG_IMPL_LEVEL_ERROR, format, __VA_ARGS__)
+#define CXXLOG_DEFINE_STATIC_CATEGORY(vname, name) _CXXLOG_IMPL_DEFINE_STATIC_CATEGORY(vname, name)
+#define CXXLOG_ERROR(logger, format, ...) _CXXLOG_IMPL_LOG(logger, _CXXLOG_IMPL_LEVEL_ERROR, format, __VA_ARGS__)
 #define CXXLOG_WARN(logger, format, ...)    _CXXLOG_IMPL_LOG(logger, _CXXLOG_IMPL_LEVEL_WARN, format, __VA_ARGS__)
 #define CXXLOG_INFO(logger, format, ...)    _CXXLOG_IMPL_LOG(logger, _CXXLOG_IMPL_LEVEL_INFO, format, __VA_ARGS__)
 
@@ -60,29 +98,39 @@ auto spdlog_useOrCreateLogger(std::string name)
 // ==========================================================================
 void example_useLogging(void)
 {
-    // auto log0 = CXXLOG_DEFINE_CATEGORY("foo.bar");
-    // auto log  = CXXLOG_DEFINE_CATEGORY("foo.bar");
-    CXXLOG_DEFINE_CATEGORY(log0, "foo.bar");
-    CXXLOG_DEFINE_CATEGORY(log,  "foo.bar");
+    CXXLOG_DEFINE_CATEGORY(log, "foo.bar");
+    // spdlog_setLevelToAll(_CXXLOG_IMPL_LEVEL_INFO);
 
     CXXLOG_ERROR0(log, "Hello Alice");
     CXXLOG_WARN(log, "Hello {}", "Bob");
     CXXLOG_INFO(log, "Hello {0} and {1}", "Alice", "Bob");
 }
 
-void spdlog_useSinkAsDefaultSink(std::shared_ptr<spdlog::logger> theLog)
+void example_useTwoLoggersWithSameName(void)
 {
-    auto defaultSink = theLog->sinks().front();
-    spdlog::apply_all([&](std::shared_ptr<spdlog::logger> logPtr) {
-        if (logPtr != theLog)
-        {
-            // -- REASSIGN: Log-sink.
-            CXXLOG_WARN0(theLog, "Reassign sink to logger");
-            logPtr->sinks().clear();
-            logPtr->sinks().push_back(defaultSink);
-        }
-    });
-} 
+    CXXLOG_DEFINE_CATEGORY(log1, "foo.two");
+    CXXLOG_DEFINE_CATEGORY(log2, "foo.two");
+
+    CXXLOG_ERROR0(log1, "Logger_1");
+    CXXLOG_WARN0(log2,  "Logger_2");
+}
+
+// std::shared_ptr<spdlog::logger>
+auto getLogger(void)
+{
+    // static CXXLOG_DEFINE_CATEGORY(theLog, "foo.static");
+    CXXLOG_DEFINE_STATIC_CATEGORY(theLog, "foo.static");
+    return theLog;
+}
+
+void example_useStaticLogger(void)
+{
+    auto log = getLogger();
+    CXXLOG_ERROR0(log, "Hello Alice");
+}
+
+
+CXXLOG_DEFINE_STATIC_CATEGORY(rootLog, "root");
 
 int main(int argc, char **argv)
 {
@@ -91,9 +139,14 @@ int main(int argc, char **argv)
     // auto console = spdlog::stdout_logger_mt("console");
     spdlog::set_level(spdlog::level::debug);
     spdlog_useSinkAsDefaultSink(console);
+    spdlog_setLevelToAll(_CXXLOG_IMPL_LEVEL_INFO);
 
     console->warn("main: Logging started.");
+    rootLog->info("main: Use static logger.root");
     example_useLogging();
+    spdlog_setLevelToAll(_CXXLOG_IMPL_LEVEL_WARN);
+    example_useTwoLoggersWithSameName();
+    example_useStaticLogger();
     console->warn("main: Ends here.");
     return 0;
 }
