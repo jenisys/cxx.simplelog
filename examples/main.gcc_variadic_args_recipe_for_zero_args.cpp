781525c219c48bd1184c006cbf63340b47f671eb
jenisys <jenisys@users.noreply.github.com>
2019-12-03 08:27:00 +0100

    Clang compiler tweaks and comment cleanups.

--- examples/main.gcc_variadic_args_recipe_for_zero_args.cpp
+++ examples/main.gcc_variadic_args_recipe_for_zero_args.cpp
@@ -1,26 +1,44 @@
 /**
  * @file main.gcc_variadic_args_recipe_for_zero_args.cpp
+ * Proof-of-concept for GNU variadic-macro trick with zero args.
  * 
- * Simple example how a generic logging framework w/ different logging 
- * subsystems could look like.
- * 
- * @see https://github.com/gabime/spdlog
- * @see https://github.com/fmtlib/fmt
+ * @par IMPLEMENTATION DETAIL: GNU CPP Preprocessor Variadic-Macro Trick
+ * The GNU CPP Preprocessor variadic-macro trick with zero args is used here.
+ * The trick eats up (deletes) a preceeding COMMA if zero __VA_ARGS__ are used.
+ * @note TRICK: ", ## __VA_ARGS__"
+ * @note NEEDS CXX COMPILER FLAG: -Wno-gnu-zero-variadic-macro-arguments
+ * @since C++2a: __VA_OPT__ (as alternative)
+ *
+ * EXAMPLE:
+ * @code
+ *  #define DIAG_PRINT(format_, ...) fmt::print(format_, ## __VA_ARGS__)
+ *
+ *  DIAG_PRINT("__ZERO_ARGS__");
+ *  DIAG_PRINT("__ONE_ARGS: {}}", "Alice");
+ *  DIAG_PRINT("__TWO_ARGS: {} and {}}", "Alice", "Bob");
+ * @endcode
+ * @see
+ *   * https://stackoverflow.com/questions/5588855/standard-alternative-to-gccs-va-args-trick
+ *   * https://gcc.gnu.org/onlinedocs/gcc/Variadic-Macros.html
+ *   * https://gcc.gnu.org/onlinedocs/cpp/Variadic-Macros.html
  **/
 
 // -- INCLUDES:
 #include <fmt/format.h>
+#ifndef SIMPLELOG_DIAG__USE_SPDLOG_FMTLIB
+#    define SIMPLELOG_DIAG__USE_SPDLOG_FMTLIB 0
+#endif
+#if SIMPLELOG_DIAG__USE_SPDLOG_FMTLIB
+#    include <spdlog/fmt/fmt.h>
+#else
+#    include <fmt/format.h>
+#endif
 #include <iostream>
 
 // ==========================================================================
 // DEFINE CPP MACROS
 // ==========================================================================
 // USING GCC-TRICK: Eat up preceeding COMMA if zero __VA_ARGS__ are used.
-// SEE: 
-//  * https://stackoverflow.com/questions/5588855/standard-alternative-to-gccs-va-args-trick
-//  * https://gcc.gnu.org/onlinedocs/gcc/Variadic-Macros.html
-//  * https://gcc.gnu.org/onlinedocs/cpp/Variadic-Macros.html
-// ALTERNATIVE SINCE C++2a: __VA_OPT__
 // REQUIRES-COMPILER-OPTION: -Wno-gnu-zero-variadic-macro-arguments (for clang)
 #define CXXLOG_1(format_, ...) \
     std::cout <<"CXXLOG_1: "<< fmt::format(format_, ## __VA_ARGS__) << std::endl
