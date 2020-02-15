dcb32ce7caff7be24ef357c2a42d179f2280bc63
jenisys <jenisys@users.noreply.github.com>
2020-02-15 20:44:39 +0100

    Replace _tasklet_cleanup.py w/ invoke_cleanup.py

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -18,7 +18,7 @@ from __future__ import absolute_import, print_function
 # BOOTSTRAP PATH: Use provided vendor bundle if "invoke" is not installed
 # -----------------------------------------------------------------------------
 from . import _setup    # pylint: disable=wrong-import-order
-INVOKE_MINVERSION = "0.14.0"
+INVOKE_MINVERSION = "1.2.0"
 _setup.setup_path()
 _setup.require_invoke_minversion(INVOKE_MINVERSION)
 
@@ -32,17 +32,17 @@ from invoke.util import cd
 from path import Path
 from contextlib import contextmanager
 
-# -- TASK-LIBRARY:
-from . import _tasklet_cleanup as cleanup
+# -----------------------------------------------------------------------------
+# TASK-LIBRARY:
+# -----------------------------------------------------------------------------
+from . import invoke_cleanup as cleanup
 from cmake_build import tasks as cmake_tasks
-# DISABLED: from . import docs
-# DISABLED: from . import test
-# DISABLED: from . import release
+
 
 # -----------------------------------------------------------------------------
 # TASKS:
 # -----------------------------------------------------------------------------
-
+# None.
 
 # -----------------------------------------------------------------------------
 # TASK CONFIGURATION:
@@ -50,9 +50,6 @@ from cmake_build import tasks as cmake_tasks
 namespace = Collection()
 namespace.add_collection(Collection.from_module(cmake_tasks, name="cmake"))
 namespace.add_collection(Collection.from_module(cleanup, name="cleanup"))
-# DISABLED: namespace.add_collection(Collection.from_module(docs))
-# DISABLED: namespace.add_collection(Collection.from_module(test))
-# DISABLED: namespace.add_collection(Collection.from_module(release))
 
 # -- INJECT: clean configuration into this namespace
 namespace.configure(cleanup.namespace.configuration())
@@ -60,8 +57,11 @@ namespace.configure(cleanup.namespace.configuration())
 # -- EXTENSTION-POINT: CLEANUP TASKS (called by: "clean" task):
 cleanup.cleanup_tasks.add_task(cleanup.clean_python)
 
+run_settings = dict(echo=True, pty=True, shell="/bin/sh")
 if sys.platform.startswith("win"):
     # -- OVERRIDE SETTINGS: For platform=win32, ... (Windows)
     from ._compat_shutil import which
     run_settings = dict(echo=True, pty=False, shell=which("cmd"))
     namespace.configure({"run": run_settings})
+
+namespace.configure({"run": run_settings})
